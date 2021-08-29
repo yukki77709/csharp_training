4d9d8e8438768e49f7da81cc1b07fcec0df12486
Степанова Юлия <kitten77709@gmail.com>
2021-08-29 10:37:00 +0400

    Домашнее задание номер 3. проект addressbook-web-test 1. выделены вспомогательные методы 2. созданы вспомогательные классы AccountData и GroupData, которые используются для инициализации и передачи тестовых данных во вспомогательные методы

--- addressbook-web-test/addressbook-web-test/GroupCreationTests.cs
+++ addressbook-web-test/addressbook-web-test/GroupCreationTests.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using addressbook_web_test;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
@@ -44,10 +45,13 @@ namespace WebAddressbookTests
         public void MetodGroupCreationTests()
         {
             OpenHomePage();
-            Login("admin", "secret");
+            Login(new AccountData ("admin", "secret") );
             GoToGroupsPage();
             InitNewGroupPage();
-            FillGroupForm("qqq", "rrr", "uuu");
+            GroupData group = new GroupData("qqq");
+            group.Header = "lll";
+            group.Footer = "yyy";
+            FillGroupForm(group);
             SubmitGroupCreation();
             ReturnGroupCreation();
             Logout();
@@ -68,17 +72,17 @@ namespace WebAddressbookTests
             driver.FindElement(By.Name("submit")).Click();
         }
 
-        private void FillGroupForm(string name, string header, string footer)
+        private void FillGroupForm(GroupData group)
         {
             driver.FindElement(By.Name("group_name")).Click();
             driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys(name);
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
             driver.FindElement(By.Name("group_header")).Click();
             driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys(header);
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
             driver.FindElement(By.Name("group_footer")).Click();
             driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys(footer);
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
         }
 
         private void InitNewGroupPage()
@@ -92,14 +96,14 @@ namespace WebAddressbookTests
             driver.FindElement(By.LinkText("groups")).Click();
         }
 
-        private void Login(string username, string password)
+        private void Login(AccountData account)
         {
             driver.FindElement(By.Name("user")).Click();
             driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys(username);
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
             driver.FindElement(By.Name("pass")).Click();
             driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys(password);
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
             driver.FindElement(By.XPath("//input[@value='Login']")).Click();
         }
 
