6f01701d9945183f8c977c676a408add9b676c38
Степанова Юлия <kitten77709@gmail.com>
2021-08-29 13:34:30 +0400

    Домашнее задание номер 4 1. в проекте  addressbook-web-test создан тест на создание нового контакта ContactCreationTests. 2. Выделины вспомогательные методы 3. создан вспомогательный класс ContactData для хранения свойств контакта (используются в методе FillContactForm) вспомогательный класс AccountData используется от предыдущего задания.

---
+++ addressbook-web-test/addressbook-web-test/ContactData.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_test
+{
+    class ContactData
+    {
+        private string firstname;
+        private string lastname;
+        private string middlename = "";
+        private string nickname = "";
+        private string title = "";
+        private string company = "";
+        private string address = "";
+        private string home = "";
+        private string mobile = "";
+        private string work = "";
+        private string fax = "";
+        private string email = "";
+        private string email2 = "";
+        private string email3 = "";
+        private string homepage = "";
+        private string bday = "";
+        private string bmonth = "";
+        private string byear = "";
+        private string aday = "";
+        private string amonth = "";
+        private string ayear = "";
+        private string new_group = "";
+        private string address2 = "";
+        private string phone2 = "";
+        private string notes = "";
+
+
+        public ContactData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+       
+
+    }
+}
